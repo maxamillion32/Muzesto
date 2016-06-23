302d394d0a980fb49ea61bb84ccc16043c7efb68
Rohan Dey <rohanoid5@gmail.com>
2016-06-23 20:39:11 +0530

    Delete Renderable.java

--- yalantis/waves/interfaces/Renderable.java
+++
@@ -1,9 +0,0 @@
-package com.rohan.app.yalantis.waves.interfaces;
-
-/**
- * Started by Artem Kholodnyi on 11/1/15 12:41 PM
- */
-public interface Renderable {
-    void render(boolean flipY);
-    void release();
-}
