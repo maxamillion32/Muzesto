03d9187aee2cc7becc8cf8b4ad33be41a232382c
Rohan Dey <rohanoid5@gmail.com>
2016-06-23 20:23:28 +0530

    Added Java Files.

---
+++ app/main/java/com/rohan/app/dataloaders/AlbumLoader.java
@@ -0,0 +1,60 @@
+
+package com.rohan.app.dataloaders;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.provider.MediaStore;
+
+import com.rohan.app.models.Album;
+import com.rohan.app.utils.PreferencesUtility;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AlbumLoader {
+
+
+    public static Album getAlbum(Cursor cursor) {
+        Album album = new Album();
+        if (cursor != null) {
+            if (cursor.moveToFirst())
+                album = new Album(cursor.getLong(0), cursor.getString(1), cursor.getString(2), cursor.getLong(3), cursor.getInt(4), cursor.getInt(5));
+        }
+        if (cursor != null)
+            cursor.close();
+        return album;
+    }
+
+
+    public static List<Album> getAlbumsForCursor(Cursor cursor) {
+        ArrayList arrayList = new ArrayList();
+        if ((cursor != null) && (cursor.moveToFirst()))
+            do {
+                arrayList.add(new Album(cursor.getLong(0), cursor.getString(1), cursor.getString(2), cursor.getLong(3), cursor.getInt(4), cursor.getInt(5)));
+            }
+            while (cursor.moveToNext());
+        if (cursor != null)
+            cursor.close();
+        return arrayList;
+    }
+
+    public static List<Album> getAllAlbums(Context context) {
+        return getAlbumsForCursor(makeAlbumCursor(context, null, null));
+    }
+
+    public static Album getAlbum(Context context, long id) {
+        return getAlbum(makeAlbumCursor(context, "_id=?", new String[]{String.valueOf(id)}));
+    }
+
+    public static List<Album> getAlbums(Context context, String paramString) {
+        return getAlbumsForCursor(makeAlbumCursor(context, "album LIKE ?", new String[]{"%" + paramString + "%"}));
+    }
+
+
+    public static Cursor makeAlbumCursor(Context context, String selection, String[] paramArrayOfString) {
+        final String albumSortOrder = PreferencesUtility.getInstance(context).getAlbumSortOrder();
+        Cursor cursor = context.getContentResolver().query(MediaStore.Audio.Albums.EXTERNAL_CONTENT_URI, new String[]{"_id", "album", "artist", "artist_id", "numsongs", "minyear"}, selection, paramArrayOfString, albumSortOrder);
+
+        return cursor;
+    }
+}
